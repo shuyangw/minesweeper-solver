a7f09b9f582940753cd9ccfbbfe9bc6bba53e370
Shuyang <qwqw1337@gmail.com>
2017-06-08 11:56:24 -0400

    Started writing the solving mechanism, added new parameters to boardnode

---
+++ func/solver.py
@@ -0,0 +1,21 @@
+import numpy as np
+import func.mvengine as mv
+
+
+#Entire board
+board = None
+
+#Self explanatory
+def update_board(brd):
+	global board
+	board = brd
+
+#This is going to take a while
+def solve():
+	pass
+
+def strat1():
+	pass
+
+def adj(node):
+	
--- func/structures/boardnode.py
+++ func/structures/boardnode.py
@@ -2,6 +2,12 @@ import numpy as np
 
 #Structure to support a 2x2 board
 class Node:
-	def __init__(self, type, coord):
+	#Constructor:
+	#	Type of node, i.e. -1 for unexplored, 0 for empty and explored and
+	#		1-8 for numbering
+	#	scr_coord, coordinate on the display
+	#	b_coord, coordinate on the board structure
+	def __init__(self, type, scr_coord, b_coord):
 		self.type = type
-		self.coord = coord
+		self.scr_coord = scr_coord
+		self.b_coord = b_coord
